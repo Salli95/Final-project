d16b6f9f5f1511a07dc3a3e44d0581e10764d17d
Salli95 <ravileviskander@gmail.com>
2023-12-12 23:06:17 +0600

    пофиксили слайдер.Но он так и не заработал

--- Final project SSS/JS/Slaider.js
+++ Final project SSS/JS/Slaider.js
@@ -1,25 +1,21 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Интервал переключения слайдов (в миллисекундах)
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+showSlides();
