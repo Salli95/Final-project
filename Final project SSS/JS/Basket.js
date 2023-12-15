6ffde93bc6777bc95ec44773aa65baa7f71c38be
Salli95 <ravileviskander@gmail.com>
2023-12-15 19:03:25 +0600

    заработала корзина на гитой стр

--- Final project SSS/JS/Basket.js
+++ Final project SSS/JS/Basket.js
@@ -1,41 +1,42 @@
-// Создаем объект корзины
-const shoppingCart = {
-    items: [],
-    total: 0,
+   // Используем JavaScript для добавления товаров в корзину
 
-    // Добавление товара в корзину
-    addItem: function (name, price) {
-        this.items.push({ name, price });
-        this.calculateTotal();
-    },
+    // Создаем объект корзины
+    const cart = [];
 
-    // Подсчет общей стоимости
-    calculateTotal: function () {
-        this.total = this.items.reduce((sum, item) => sum + item.price, 0);
-        this.updateCartUI();
-    },
+    // Функция для добавления товара в корзину
+    function addToCart(itemName, price) {
+        const item = {
+            name: itemName,
+            price: price
+        };
 
-    // Обновление интерфейса корзины
-    updateCartUI: function () {
-        // Ваш код для обновления интерфейса корзины, например, вывод общей стоимости в какой-то элемент на странице
-        console.log(`Total: $${this.total}`);
+        // Добавляем товар в корзину
+        cart.push(item);
+
+        // Обновляем отображение корзины
+        updateCartDisplay();
     }
-};
 
-// Функция для добавления товара в корзину при нажатии кнопки
-function addToCart(name, price) {
-    shoppingCart.addItem(name, price);
-}
+    // Функция для обновления отображения корзины
+    function updateCartDisplay() {
+        // Находим элемент, где будет отображаться корзина (предположим, это div с id "cartTotal")
+        const cartTotalElement = document.getElementById('cartTotal');
 
-// Обработчики событий для кнопок "В корзину"
-document.querySelectorAll('.card__add').forEach(button => {
-    button.addEventListener('click', function () {
-        // Получаем информацию о товаре из родительской карточки
-        const card = this.closest('.card');
-        const title = card.querySelector('.card__title').textContent;
-        const discountPrice = parseFloat(card.querySelector('.card__price--discount').textContent);
-        
-        // Вызываем функцию добавления товара в корзину
-        addToCart(title, discountPrice);
+        // Вычисляем общую стоимость товаров в корзине
+        const total = cart.reduce((acc, item) => acc + item.price, 0);
+
+        // Обновляем текст в элементе
+        cartTotalElement.textContent = `Общая стоимость: $${total.toFixed(2)}`;
+    }
+
+    // Добавляем обработчики клика на кнопки "В корзину"
+    const addToCartButtons = document.querySelectorAll('.card__add');
+    addToCartButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const card = this.closest('.card');
+            const itemName = card.querySelector('.card__title').textContent;
+            const price = parseFloat(card.querySelector('.card__price--discount').textContent);
+
+            addToCart(itemName, price);
+        });
     });
-});
